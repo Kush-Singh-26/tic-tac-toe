f0ddbc0f28d9b8ddcc1d360d617f8e018b34d66d
Kush-Singh-26 <kushsingh2004+webdev@gmail.com>
2024-06-03 01:07:33 +0530

    name is displaying

--- script.js
+++ script.js
@@ -1,37 +1,42 @@
-const GameBoardModule = (function() {
+const GameBoardModule = (function () {
     const gameboard = ["", "", "", "", "", "", "", "", ""];
 
-    const setField = (index,sign) => {
+    const setField = (index, sign) => {
         gameboard[index] = sign;
     }
 
     const getField = (index) => {
         return gameboard[index];
     }
-    return{gameboard, setField, getField };
+    return { gameboard, setField, getField };
 })();
 
-const displayController = (function(){
+const displayController = (function () {
     const cells = document.querySelectorAll(".grid-box");
     const submitbtn = document.querySelector("#submit");
     const main = document.querySelector("main");
-    let playerone; 
+    let playerone;
     let playertwo;
     let currentPlayer = "X";
     const dialog = document.querySelector(".dialog");
-    submitbtn.addEventListener('click', (e)=>{
+    const player1 = document.querySelector(".info-1");
+    const player2 = document.querySelector(".info-2");
+
+    submitbtn.addEventListener('click', (e) => {
         e.preventDefault();
         playerone = document.querySelector("#playerone").value;
         playertwo = document.querySelector("#playertwo").value;
+        player1.textContent = playerone;
+        player2.textContent = playertwo;
         main.classList.remove("main-hide");
         main.classList.add("main-show");
         dialog.style.display = "none";
     })
-    cells.forEach((cell) =>{
-        cell.addEventListener('click', (e) =>{
+    cells.forEach((cell) => {
+        cell.addEventListener('click', (e) => {
             const index = parseInt(e.target.dataset.index);
-            if(GameBoardModule.getField(index) === ""){
-                GameBoardModule.setField(index,currentPlayer);
+            if (GameBoardModule.getField(index) === "") {
+                GameBoardModule.setField(index, currentPlayer);
                 e.target.textContent = currentPlayer;
                 currentPlayer = currentPlayer === "X" ? "O" : "X";
             }
@@ -39,10 +44,10 @@ const displayController = (function(){
         })
     })
 
-    return{};
+    return {};
 })
 
-const gameController = ( ()=>{
+const gameController = (() => {
 
 });
 displayController();
